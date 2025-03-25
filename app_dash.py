fa56cb2b8d2d13025ee53ef410366c46ca6db849
Leonardo Leite Cruz <133298648+devleocruz@users.noreply.github.com>
2025-03-25 11:23:56 -0400

    Update app_dash.py

--- app_dash.py
+++ app_dash.py
@@ -161,9 +161,6 @@ def atualizar_grafo(modo, dimensao):
 
 app.layout = gerar_layout()
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
     app.run(host="0.0.0.0", port=port, debug=True)
